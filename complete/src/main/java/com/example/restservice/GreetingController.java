44fe639e978eb83158f5530f2e69caf826cd4d5e
romasi82 <70348799+romasi82@users.noreply.github.com>
2021-03-08 16:38:29 -0800

    Add files via upload

---
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,28 @@
+package com.example.restservice;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "%s";
+
+	@Autowired
+	private ServerProperties serverProperties;
+
+	@GetMapping("/")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws UnknownHostException {
+		InetAddress ip = InetAddress.getLocalHost();
+		int serverPort = serverProperties.getPort();
+		return new Greeting(String.format(template, ip.getHostAddress() + ":" + String.valueOf(serverPort)));
+	}
+}
